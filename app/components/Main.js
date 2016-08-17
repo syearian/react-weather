ca359fc1c2011f9b30d9201096d8d6a8f45ef15d
Steven Yearian <syearian@gmail.com>
2016-08-17 15:15:44 -0500

    Add header to Main.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,8 +3,13 @@ var React = require('react');
 var Main = React.createClass({
   render: function () {
     return (
-      <div className='main-container'>
-        {this.props.children}
+      <div>
+        <header>
+          <h1>Weather App</h1>
+        </header>
+        <main className='main-container'>
+          {this.props.children}
+        </main>
       </div>
     )
   }
