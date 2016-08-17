2c2eb7adf93d4e8f7a2c777f8ad3d31e42151fa6
Steven Yearian <syearian@gmail.com>
2016-08-17 12:04:02 -0500

    Remove uneeded code from routess.js

--- app/config/routes.js
+++ app/config/routes.js
@@ -11,8 +11,6 @@ var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
-      <Route path='playerOne' header='Player One' component={PromptContainer} />
-      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
     </Route>
   </Router>
 );
