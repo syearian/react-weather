2decf0224acbbd3448275a3cc8b5df2630b314ee
Steven Yearian <syearian@gmail.com>
2016-08-23 15:00:21 -0500

    Move GetWeatherButton into CityInput

--- app/components/CityInput.js
+++ app/components/CityInput.js
@@ -1,16 +1,32 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link
 
 function CityInput (props) {
   return (
-    <div className="form-group">
-      <input
-        className='form-control'
-        onChange={props.onUpdateUser}
-        placeholder='Dallas, TX'
-        type='text'
-        value={props.username} />
-    </div>
+    <form className={props.className} onSubmit={props.onSumbitLocation}>
+      <div className="form-group">
+        <input
+          className='form-control'
+          onChange={props.onUpdatelocation}
+          placeholder='Dallas, TX'
+          type='text'
+          value={props.location} />
+      </div>
+      <Link to='/forecast'>
+        <button type='submit' className='btn btn-success'
+          style={props.style.button}>Get Weather</button>
+      </Link>
+    </form>
   );
 }
+
+CityInput.propTypes = {
+  className: PropTypes.string,
+  onSubmitLocation: PropTypes.func.isRequired,
+  onUpdateLocation: PropTypes.func.isRequired,
+  location: PropTypes.string.isRequired,
+  style: PropTypes.object
+};
 
 module.exports = CityInput;
