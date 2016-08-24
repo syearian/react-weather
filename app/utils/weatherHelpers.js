ff5d7ffa86ea6663f2b01d08a30921fa780577bf
Steven Yearian <syearian@gmail.com>
2016-08-24 13:43:27 -0500

    Add weatherHelper functions

--- app/utils/weatherHelpers.js
+++ app/utils/weatherHelpers.js
@@ -1,1 +1,91 @@
 var axios = require('axios');
+
+const APPID = '&APPID=cfc2eaa1c51253a29ce7206e1aad37c9';
+
+function getCurrentWeather(city, country) {
+  return axios.get('https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&units=imperial' + APPID).then(function (data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+function getForecast(city, country) {
+  return axios.get('https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + ',' + country + '&units=imperial' + APPID + '&cnt=5').then(function (data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+// capitalize a string
+function capitalize(str) { 
+  var first = str[0].slice(0, 1); 
+  first = first.toUpperCase(); 
+  var rest = str.slice(1); 
+  rest = rest.toLowerCase(); 
+  return first + rest; 
+}
+
+// Set value of city, country, and format location
+function getLocation(value) {
+  location = value;
+  var array = location.split(",");
+  city = array[0];
+  city = capitalize(toLowerCase(city));
+  country = array[1].trim();
+  location = city + ', ' + country.toUpperCase();
+  country = country.toLowerCase();
+}
+
+// Change the background image
+function changeBackImg(group) {
+  var backImages = {
+    "Thunderstorm" : "img/thunderstorm.jpg",
+    "Drizzle" : "img/drizzle.jpg",
+    "Rain" : "img/rain.jpg",
+    "Snow" : "img/snow.jpg",
+    "Atmosphere" : "img/atmosphere.jpg",
+    "Clear" : "img/clear.jpg",
+    "Clouds" : "img/clouds.jpg",
+  };
+  var backImg = "";
+  switch (group) {
+    case "Thunderstorm":
+      backImg = backImages.Thunderstorm;
+      break;
+    case "Drizzle":
+      backImg = backImages.Drizzle;
+      break;
+    case "Rain":
+      backImg = backImages.Rain;
+      break;
+    case "Snow":
+      backImg = backImages.Snow;
+      break;
+    case "Atmosphere":
+      backImg = backImages.Atmosphere;
+      break;
+    case "Clear":
+      backImg = backImages.Clear;
+      break;
+    case "Clouds":
+      backImg = backImages.Clouds;
+      break;
+  }
+}
+
+var helpers = {
+  getCurrentWeather: function(value) {
+    var city, country, location;
+    getLocation(value);
+    getCurrentWeather(value);
+  },
+  getForecast: function(value) {
+    var city, country, location;
+    getLocation(value);
+    getForecast(value);
+  }
+}
