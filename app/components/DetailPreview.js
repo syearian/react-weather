3eb564a3e84e28d1dc06a8b197fd24f163ae0f70
Steven Yearian <syearian@gmail.com>
2016-08-25 13:35:39 -0500

    Format temp in DetailPreviews

--- app/components/DetailPreview.js
+++ app/components/DetailPreview.js
@@ -4,12 +4,13 @@ var utils = require('../utils/utils');
 
 function DetailPreview (props) {
   var date = utils.getDate(props.day.dt);
+  var high = Math.round(props.day.temp.max);
   return (
     <div className={props.className}>
       <h4>{date}</h4>
       <p>{console.log(props.day)}</p>
       <p>{props.day.weather[0].main}</p>
-      <p>Temp: {props.day.temp.max}/{props.day.temp.min}</p>
+      <p>High: {high}</p>
     </div>
   );
 }
